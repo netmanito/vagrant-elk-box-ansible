e33c2bd1e7390e686c707a6ca1db46c387d573fa
Jacinto Calvo Sintes <jacinto@seriousman.org>
2016-04-07 16:45:46 +0200

    fixed send_log script

--- include/example-logs/send_log.sh
+++ include/example-logs/send_log.sh
@@ -5,9 +5,9 @@
 #while true
 #do
 
-message='{"@timestamp":"'`date -u "+%Y-%m-%dT%H:%M:%S.%s"`'+01:00","@fields":{"level":"INFO","logger":"queryLog","properties":{"log4net:HostName":"hostMachine"},"exception":null},"@message":{"documentTitle":"Proving that Android’s, Java’s and Python’s sorting algorithm is broken (and showing how to fix it)"}}"' 
+message='{"@timestamp":"'`date -u "+%Y-%m-%dT%H:%M:%S.%s"`'+01:00","@fields":{"level":"INFO","logger":"queryLog","properties":{"log4net:HostName":"hostMachine"},"exception":null},"@message":{"documentTitle":"Proving that Android’s, Java’s and Python’s sorting algorithm is broken (and showing how to fix it)"}}'
 
-cat $message > testlog
+echo $message > testlog
 
 #sleep 15
 #done
